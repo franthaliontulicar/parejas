d7ee8bb7f2dc784cfbe5a593f4ef41710647c3c2
paco <franmenendezpouso@gmail.com>
2016-01-29 13:31:56 +0100

    actividad 197 incompleta

--- Emparejamiento.java
+++ Emparejamiento.java
@@ -12,43 +12,47 @@ public class Emparejamiento
     // instance variables - replace the example below with your own
     private ArrayList<Alumno> alumnos;
     private ArrayList<Alumno> pareja;
-    
+
     public Emparejamiento(){
         alumnos = new ArrayList<Alumno>();
         pareja =  new ArrayList<Alumno>();
-    
+
     }
-    
+
     public void añadirAlumnos(Alumno alumno){
-       int numeroAlum = 0;
+        int numeroAlum = 0;
         while(numeroAlum < alumnos.size()){
             if(numeroAlum >= 2 ){
-            alumnos.add(alumno);
-            
+                alumnos.add(alumno);
+
             }
-            
+
         }
-    
+
     }
-    
+
     public void mostrarAlumnos(Alumno alumno){
         for(Alumno dentro : alumnos){
-            alumno.getDatos();
+            dentro.getDatos();
         }
-    
+        System.out.println(alumno.getNombre()+alumno.getApellido1()+ alumno.getApellido2()); 
     }
-    
-    public void seleccionarGrupo(Alumno alumno){
-       
+
+    public void seleccionarGrupo(){
+
         boolean impar = false;
+        Collections.shuffle(pareja);
+        for(Alumno grupo : pareja){
+            if(impar){
+                pareja.add(grupo);
+            }
+            else{
+                System.out.println("El numero de alumnos es impar, asi que habra una pareja de tres");
+            }
+            grupo.getDatos();
+
+        }
         
-        if(impar){
-            pareja.add(alumno);
-        }
-        else{
-            System.out.println("El numero de alumnos es impar, asi que habra una pareja de tres");
-        }
-    
     }
-    
+
 }
